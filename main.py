5789990f7f890de1afc1ace056a51e9a4b1fe25f
Evgen2000 <79366759+Evgen2000@users.noreply.github.com>
2024-06-11 11:12:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import asyncio
-from aiogram import Bot, Dispatcher, F
-
-from handlers import router
-
+import asyncio  # Модуль для работы с асинхронным программированием
+from aiogram import Bot, Dispatcher, F  # Импорт классов Bot, Dispatcher и F из библиотеки aiogram
+from handlers import router  # Импорт маршрутизатора из модуля handlers
 
 async def main():
+    # Создаем объект бота с указанием токена
     bot = Bot(token="6429052985:AAFyrq1rR1-ewwPb3X80uB7sFP082dsnOXw")
+    
+    # Создаем диспетчер для обработки событий
     dp = Dispatcher()
+    
+    # Включаем маршрутизатор, который содержит обработчики событий
     dp.include_router(router)
+    
+    # Запускаем процесс polling для получения обновлений от Telegram
     await dp.start_polling(bot)
-
 
 if __name__ == '__main__':
     try:
+        # Запускаем основную асинхронную функцию
         asyncio.run(main())
     except KeyboardInterrupt:
+        # Если выполнение прерывается (например, нажатием Ctrl+C), выводим сообщение
         print('Бот выключен')
